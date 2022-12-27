56f4b8e3bf9c6534cbd8d9c3b24b5dcc1f0cae11
burrozd <95920902+burrozd@users.noreply.github.com>
2022-12-28 00:28:08 +0100

    cleaning

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const hamburgerButton = document.getElementById("popupButton");
 const hamburgerMenu = document.getElementById("menuPopup");
 const closeButton = document.getElementById("closeButton");
 
-/* hamburgerButton.addEventListener("click", function() {
-    hamburgerMenu.style.display = "flex";
-}); */
-
 hamburgerButton.addEventListener("click", () => {
     hamburgerMenu.style.display = "flex";
     hamburgerMenu.classList.add("popped");
@@ -14,31 +10,3 @@ hamburgerButton.addEventListener("click", () => {
 closeButton.addEventListener("click", function() {
     hamburgerMenu.style.display = "none";
 });
-
-/* function fadeIn(el) {
-    el.style.opacity = 0;
-  
-    var tick = function() {
-      el.style.opacity = +el.style.opacity + 0.01;
-  
-      if (+el.style.opacity < 1) {
-        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
-      }
-    };
-  
-    tick();
-  }
-  
-  document.getElementById('popupButton').addEventListener('click', function() {
-    fadeIn(document.getElementById('element'));
-  }); */
-
-/* const menuFadein = [
-    { transform: "rotate(0) scale(1)" },
-    { transform: "rotate(360deg) scale(0)" },
-];
-
-const fadeinTiming = {
-    duration: 2000,
-    iterations: 1,
-}; */
