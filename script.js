59221e0873f6c6efbb08fd7de3a551d742c37b6d
burrozd <95920902+burrozd@users.noreply.github.com>
2022-12-27 02:00:59 +0100

    javascript mobile menu

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-const button = document.getElementById("#hamburgerButton");
+const hamburgerButton = document.getElementById("popupButton");
+const hamburgerMenu = document.getElementById("menuPopup");
+const closeButton = document.getElementById("closeButton");
 
-let menuModal = document.createElement("div");
-menuModal.className = "menu-modal";
+hamburgerButton.addEventListener("click", function() {
+    hamburgerMenu.style.display = "flex";
+});
 
-let online = document.createElement("h1");
-online.className = "menu-online";
-
-let onSite = document.createElement("h1");
-onSite.className = "menu-onsite";
-
-let story = document.createElement("h1");
-story.className = "menu-story";
-
-let contact = document.createElement("h1");
-contact.className = "menu-contact";
-
-let menuClose = document.createElement("button");
-menuClose.className = "close-button";
-
-function menuModal() {
-    addEventListener("click", menuModal);
-    menuModal.style.display = "flex";
-}
+closeButton.addEventListener("click", function() {
+    hamburgerMenu.style.display = "none";
+});
